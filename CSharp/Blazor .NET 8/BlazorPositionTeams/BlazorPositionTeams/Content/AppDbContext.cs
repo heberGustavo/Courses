836c8088a3e41cd39a243fa294d7ef9d64d97748
heberGustavo <heber.gbarbosa@hotmail.com>
2024-02-01 09:04:13 -0300

    List teams - Using Blazor 8.0, EF Core, Quick Grid and Filter

---
+++ CSharp/Blazor .NET 8/BlazorPositionTeams/BlazorPositionTeams/Content/AppDbContext.cs
@@ -0,0 +1,78 @@
+﻿using BlazorPositionTeams.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorPositionTeams.Content
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+        public DbSet<Team> Teams { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Team>().HasData(
+				new Team
+				{
+					Id = 1,
+					Name = "Botafogo",
+					Games = 32,
+					Points = 65,
+					Victories = 30,
+					Draws = 1,
+					Defeats = 1,
+					GoalDifference = 60,
+					GoalsScored = 90,
+					HasMundial = false,
+					Status = "Libertadores"
+				},
+				new Team
+				{
+					Id = 2,
+					Name = "Palmeiras",
+					Games = 30,
+					Points = 60,
+					Victories = 20,
+					Draws = 5,
+					Defeats = 5,
+					GoalDifference = 60,
+					GoalsScored = 90,
+					HasMundial = true,
+					Status = "Libertadores"
+				},
+				new Team
+				{
+					Id = 3,
+					Name = "São Paulo",
+					Games = 32,
+					Points = 50,
+					Victories = 30,
+					Draws = 1,
+					Defeats = 1,
+					GoalDifference = 60,
+					GoalsScored = 90,
+					HasMundial = true,
+					Status = "Sulamericana"
+				},
+				new Team
+				{
+					Id = 4,
+					Name = "Atletico Paranaense",
+					Games = 15,
+					Points = 10,
+					Victories = 5,
+					Draws = 1,
+					Defeats = 1,
+					GoalDifference = 60,
+					GoalsScored = 90,
+					HasMundial = false,
+					Status = "Rebaixamento"
+				}
+			);
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ CSharp/Blazor .NET 8/BlazorPositionTeams/BlazorPositionTeams/Entities/Team.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorPositionTeams.Entities
+{
+	public class Team
+	{
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Points { get; set; }
+        public int Games { get; set; }
+        public int Victories { get; set; }
+        public int Draws { get; set; }
+        public int Defeats { get; set; }
+        public int GoalDifference { get; set; }
+        public int GoalsScored { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public bool HasMundial { get; set; }
+    }
+}
