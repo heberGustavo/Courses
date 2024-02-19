2794f4ea2239c760cbeac94a9f96ce8b924ea742
heberGustavo <heber.gbarbosa@hotmail.com>
2024-02-19 07:48:52 -0300

    Creating entity and adjusting the listing of data

---
+++ CSharp/Blazor .NET 8/ManageTasks/ManageTasks/Entities/MyTask.cs
@@ -0,0 +1,10 @@
+﻿namespace ManageTasks.Entities
+{
+	public class MyTask
+	{
+		public Guid Id { get; set; }
+		public string Description { get; set; }
+		public bool Completed { get; set; }
+		public DateTime DateCreation { get; set; }
+	}
+}
