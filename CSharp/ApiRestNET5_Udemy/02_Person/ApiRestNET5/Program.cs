8134f4980faf529a410224ccf88cd428919d99c3
heberGustavo <heber.gbarbosa@hotmail.com>
2024-03-23 06:55:38 -0300

    Creating connection with Database

--- CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Program.cs
+++ CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Program.cs
@@ -1,8 +1,21 @@
+using ApiRestNET5.Migration;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+#region MySQL
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLConnection>(options => options.UseMySql(
+	connection,
+	new MySqlServerVersion(new Version(8,0,31))
+	));
+
+#endregion
 
 var app = builder.Build();
 
