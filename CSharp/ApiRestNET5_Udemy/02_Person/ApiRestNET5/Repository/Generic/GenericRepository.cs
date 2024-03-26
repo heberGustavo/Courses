c0fac41092381a753efd34eeb267e32fa97a751b
heberGustavo <heber.gbarbosa@hotmail.com>
2024-03-26 06:49:45 -0300

    Implementing Repository Generic

---
+++ CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,83 @@
+﻿using ApiRestNET5.Model;
+using ApiRestNET5.Model.Base;
+using ApiRestNET5.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiRestNET5.Repository.Generic
+{
+	public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+	{
+		private MySQLContext _context;
+		private DbSet<T> _dataset;
+
+		public GenericRepository(MySQLContext context)
+		{
+			_context = context;
+			_dataset = context.Set<T>();
+		}
+
+		#region Read
+
+		public List<T> FindAll() => _dataset.ToList();
+
+		public T FindById(long id) => _dataset.SingleOrDefault(p => p.Id.Equals(id));
+
+		#endregion
+
+		public T Create(T item)
+		{
+			try
+			{
+				_dataset.Add(item);
+				_context.SaveChanges();
+				return item;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+
+		public void Delete(long id)
+		{
+			try
+			{
+				var item = _dataset.SingleOrDefault(b => b.Id == id);
+				if (item != null)
+				{
+					_dataset.Remove(item);
+					_context.SaveChanges();
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+
+		public T Update(T item)
+		{
+			var result = _dataset.SingleOrDefault(b => b.Id == item.Id);
+			if (result != null)
+			{
+				try
+				{
+					_context.Entry(result).CurrentValues.SetValues(item);
+					_context.SaveChanges();
+					return item;
+				}
+				catch (Exception)
+				{
+					throw;
+				}
+			}
+			else
+				return null;
+		}
+		
+		public bool Exists(long id)
+		{
+			return _dataset.Any(p => p.Id.Equals(id));
+		}
+	}
+}
---
+++ CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Repository/Generic/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using ApiRestNET5.Model;
+using ApiRestNET5.Model.Base;
+
+namespace ApiRestNET5.Repository.Generic
+{
+	public interface IRepository<T> where T : BaseEntity
+	{
+		List<T> FindAll();
+		T FindById(long id);
+
+		T Create(T item);
+		T Update(T item);
+		void Delete(long id);
+
+		bool Exists(long id);
+	}
+}
