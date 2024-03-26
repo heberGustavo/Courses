8051fda84a8af822b5addd8cdf23a2431f9d1a9a
heberGustavo <heber.gbarbosa@hotmail.com>
2024-03-26 16:43:16 -0300

    Implementing converter/parse to model and VO

---
+++ CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Data/Converter/Contract/IParser.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiRestNET5.Data.Converter.Contract
+{
+	public interface IParser<O,D>
+	{
+		D Parser(O origem);
+		List<D> Parser(List<O> origem);
+	}
+}
---
+++ CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Data/Converter/Implementations/PersonConverter.cs
@@ -0,0 +1,56 @@
+﻿using ApiRestNET5.Data.Converter.Contract;
+using ApiRestNET5.Data.VO;
+using ApiRestNET5.Model;
+
+namespace ApiRestNET5.Data.Converter.Implementations
+{
+	public class PersonConverter : IParser<PersonVO, Person>, IParser<Person, PersonVO>
+	{
+		#region Return Person 
+
+		public Person Parser(PersonVO origem)
+		{
+			if (origem == null) return null;
+			return new Person
+			{
+				Id = origem.Id,
+				FirstName = origem.FirstName,
+				LastName = origem.LastName,
+				Gender = origem.Gender,
+				Address = origem.Address
+			};
+		}
+
+		public List<Person> Parser(List<PersonVO> origem)
+		{
+			if (origem == null) return null;
+			return origem.Select(item => Parser(item)).ToList();
+		}
+
+		#endregion
+		
+		#region Return PersonVO
+
+		public PersonVO Parser(Person origem)
+		{
+			if (origem == null) return null;
+			return new PersonVO
+			{
+				Id = origem.Id,
+				FirstName = origem.FirstName,
+				LastName = origem.LastName,
+				Gender = origem.Gender,
+				Address = origem.Address
+			};
+		}
+
+		public List<PersonVO> Parser(List<Person> origem)
+		{
+			if (origem == null) return null;
+			return origem.Select(item => Parser(item)).ToList();
+		}
+
+		#endregion
+
+	}
+}
