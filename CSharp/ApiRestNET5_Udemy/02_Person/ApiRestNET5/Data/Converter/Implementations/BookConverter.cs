eb7cf45cb6f11db0301ef567355d320fb3020a19
heberGustavo <heber.gbarbosa@hotmail.com>
2024-03-27 06:22:29 -0300

    Creating BookConverter

---
+++ CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Data/Converter/Implementations/BookConverter.cs
@@ -0,0 +1,55 @@
+﻿using ApiRestNET5.Data.Converter.Contract;
+using ApiRestNET5.Data.VO;
+using ApiRestNET5.Model;
+
+namespace ApiRestNET5.Data.Converter.Implementations
+{
+	public class BookConverter : IParser<BookVO, Book>, IParser<Book, BookVO>
+	{
+		#region Return Book
+
+		public Book Parser(BookVO origem)
+		{
+			if (origem == null) return null;
+			return new Book
+			{
+				Id = origem.Id,
+				Author = origem.Author,
+				Title = origem.Title,
+				Price = origem.Price,
+				LaunchDate = origem.LaunchDate
+			};
+		}
+
+		public List<Book> Parser(List<BookVO> origem)
+		{
+			if (origem == null) return null;
+			return origem.Select(item => Parser(item)).ToList();
+		}
+
+		#endregion
+
+		#region Return BookVO
+
+		public BookVO Parser(Book origem)
+		{
+			if (origem == null) return null;
+			return new BookVO
+			{
+				Id = origem.Id,
+				Author = origem.Author,
+				Title = origem.Title,
+				Price = origem.Price,
+				LaunchDate = origem.LaunchDate
+			};
+		}
+
+		public List<BookVO> Parser(List<Book> origem)
+		{
+			if (origem == null) return null;
+			return origem.Select(item => Parser(item)).ToList();
+		}
+
+		#endregion
+	}
+}
---
+++ CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Data/VO/BookVO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiRestNET5.Data.VO
+{
+	public class BookVO
+	{
+		public int Id { get; set; }
+		public string Author { get; set; }
+		public DateTime LaunchDate { get; set; }
+		public decimal Price { get; set; }
+		public string Title { get; set; }
+	}
+}
