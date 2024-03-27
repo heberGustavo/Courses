15d5f61edc5dd59b4d7cac1531034b74ca26d0f4
heberGustavo <heber.gbarbosa@hotmail.com>
2024-03-27 06:47:54 -0300

    Serialization test

--- CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Data/VO/BookVO.cs
+++ CSharp/ApiRestNET5_Udemy/02_Person/ApiRestNET5/Data/VO/BookVO.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ApiRestNET5.Data.VO
 {
 	public class BookVO
 	{
+		//[JsonPropertyName("name_custom_id")]
 		public int Id { get; set; }
 		public string Author { get; set; }
 		public DateTime LaunchDate { get; set; }
 		public decimal Price { get; set; }
+		//[JsonIgnore] //Ignore in serialization
 		public string Title { get; set; }
 	}
 }
