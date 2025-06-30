495048560357a2f8b4c9b55b873164041a3daec1
heberGustavo <heber.gbarbosa@hotmail.com>
2025-06-30 08:30:49 -0300

    StringType

---
+++ TypeScript/modulo-02/stringType/stringType.js
@@ -0,0 +1,11 @@
+"use strict";
+// ===> Exemplo - Single Quotes
+let nomeCompleto = 'Hebeer Gustavo';
+console.log(nomeCompleto);
+// ===> Exemplo - Double Quotes
+let nomeAbreviado = "Heber G B da Cruz";
+console.log(nomeAbreviado);
+// ===> Exemplo - Back Ticks
+let primeiroNomeH = "Heber";
+let ultimoNomeH = 'Cruz';
+console.log(`Meu nome é: ${primeiroNomeH} da ${ultimoNomeH}`);
---
+++ TypeScript/modulo-02/stringType/stringType.ts
@@ -0,0 +1,14 @@
+
+// ===> Exemplo - Single Quotes
+let nomeCompleto: string = 'Hebeer Gustavo';
+console.log(nomeCompleto);
+
+// ===> Exemplo - Double Quotes
+let nomeAbreviado: string = "Heber G B da Cruz";
+console.log(nomeAbreviado);
+
+// ===> Exemplo - Back Ticks
+let primeiroNomeH: string = "Heber";
+let ultimoNomeH: string = 'Cruz';
+
+console.log(`Meu nome é: ${primeiroNomeH} da ${ultimoNomeH}`);
