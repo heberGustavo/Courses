ee1e2f8c06f905030134b4ce067d7f28116947d7
heberGustavo <heber.gbarbosa@hotmail.com>
2025-06-30 08:04:16 -0300

    BooleanType

---
+++ TypeScript/modulo-02/booleanType/booleanType.js
@@ -0,0 +1,5 @@
+"use strict";
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(`A tarefa concluída esta com status: ${tarefaConcluida}`);
+console.log(`A tarefa pendente esta com status: ${tarefaPendente}`);
---
+++ TypeScript/modulo-02/booleanType/booleanType.ts
@@ -0,0 +1,6 @@
+
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(`A tarefa concluída esta com status: ${tarefaConcluida}`);
+console.log(`A tarefa pendente esta com status: ${tarefaPendente}`);
