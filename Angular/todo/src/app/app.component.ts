e4267a9307e31d23c39e9f250625c55edd53c227
heberGustavo <heber.gbarbosa@hotmail.com>
2025-07-24 12:31:46 -0300

    Criar tela inicial, funcionalidades Concluir, Refazer, Remover e Recarregar

--- Angular/todo/src/app/app.component.ts
+++ Angular/todo/src/app/app.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: string = "Minhas tarefas";
+
+  constructor() {
+    this.todos.push(new Todo(1, "Cortar o cabelo", false));    
+    this.todos.push(new Todo(2, "Passear com cachorro", false));
+    this.todos.push(new Todo(3, "Ir ao supermercado", true));
+  }
+
+  reloadScreen(){
+    window.location.reload();
+  }
+
+  remove(todo: Todo){
+    let index = this.todos.indexOf(todo);
+    if (index != 1)
+      this.todos.splice(index, 1);
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+  }
+
 }
