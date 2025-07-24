548c14e7addf6a31c6358ce925fe57f79418522c
heberGustavo <heber.gbarbosa@hotmail.com>
2025-07-24 13:03:49 -0300

    Criar formulario para salvar tarefas

--- Angular/todo/src/app/app.component.ts
+++ Angular/todo/src/app/app.component.ts
@@ -1,26 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   public todos: Todo[] = [];
   public title: string = "Minhas tarefas";
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const idProximoItem = this.todos.length + 1;
+
+    this.todos.push(new Todo(idProximoItem, title, false));
+    this.clearForm();
+  }
+
+  addMook(){
     this.todos.push(new Todo(1, "Cortar o cabelo", false));    
     this.todos.push(new Todo(2, "Passear com cachorro", false));
     this.todos.push(new Todo(3, "Ir ao supermercado", true));
-  }
-
-  reloadScreen(){
-    window.location.reload();
   }
 
   remove(todo: Todo){
@@ -37,4 +52,11 @@ export class AppComponent {
     todo.done = false;
   }
 
+  clearForm(){
+    this.form.reset();
+  }
+
+  reloadScreen(){
+    window.location.reload();
+  }
 }
