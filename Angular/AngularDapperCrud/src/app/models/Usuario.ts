1c9f481c1a976deeee9402b87192c3fcb716bb05
heberGustavo <heber.gbarbosa@hotmail.com>
2025-07-30 14:41:06 -0300

    Criar models

---
+++ Angular/AngularDapperCrud/src/app/models/Usuario.ts
@@ -0,0 +1,10 @@
+export interface UsuarioListar {
+    id?: number;
+    nomeComplete: string;
+    email: string;
+    cargo: string;
+    salario: number;
+    situacao: boolean;
+    cpf: string;
+    senha: string; 
+}
