563863ba40f207b25c7c7b56d9f9fa24c7cb9647
heberGustavo <heber.gbarbosa@hotmail.com>
2025-08-21 09:57:03 -0300

    Interpolação

--- Angular/curso-angular/src/app/components/first-component/first-component.component.ts
+++ Angular/curso-angular/src/app/components/first-component/first-component.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './first-component.component.css'
 })
 export class FirstComponentComponent {
-
+  name: string = "Heber";
+  age: number = 27;
+  job: string = "Dev Full Stack";
+  hobbies = ['Correr', 'Jogar', 'Estudar'];
+  car = {
+    name: "Civic",
+    year: 2022
+  }
 }
