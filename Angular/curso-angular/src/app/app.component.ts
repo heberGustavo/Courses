1b693cf455fc0b9e2ee19a573dadabe5657c4a6b
heberGustavo <heber.gbarbosa@hotmail.com>
2025-08-27 09:20:25 -0300

    Router

--- Angular/curso-angular/src/app/app.component.ts
+++ Angular/curso-angular/src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ParentDataComponent } from "./components/parent-data/parent-data.component";
-import { DirectivesComponent } from "./components/directives/directives.component";
-import { IfRenderComponent } from "./components/if-render/if-render.component";
-import { EventsComponent } from "./components/events/events.component";
-import { EmitterComponent } from "./components/emitter/emitter.component";
-import { ListRenderComponent } from "./components/list-render/list-render.component";
-import { PipesComponent } from "./components/pipes/pipes.component";
-import { TwoWayBindingComponent } from "./components/two-way-binding/two-way-binding.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent, EventsComponent, EmitterComponent, ListRenderComponent, PipesComponent, TwoWayBindingComponent],
+  imports: [RouterOutlet, RouterLink, ParentDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Angular/curso-angular/src/app/app.routes.ts
+++ Angular/curso-angular/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { EventsComponent } from './components/events/events.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: FirstComponentComponent },
+    { path: 'list', component: ListRenderComponent },
+    { path: 'change-number', component: ChangeNumberComponent },
+    { path: 'directives', component: DirectivesComponent },
+    { path: 'emitter', component: EmitterComponent },
+    { path: 'events', component: EventsComponent },
+    { path: 'if-render', component: IfRenderComponent },
+    { path: 'pipes', component: PipesComponent },
+    { path: 'two-way-data-binding', component: TwoWayBindingComponent },
+];
