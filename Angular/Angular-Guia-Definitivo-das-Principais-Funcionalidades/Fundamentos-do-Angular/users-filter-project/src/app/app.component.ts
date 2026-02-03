aad13e3a285bf94ae0198756742d81077f144e88
heberGustavo <heber.gbarbosa@hotmail.com>
2026-02-03 07:11:50 -0300

    Create functions to filter users

--- Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Fundamentos-do-Angular/users-filter-project/src/app/app.component.ts
+++ Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Fundamentos-do-Angular/users-filter-project/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/users/user.interface';
 import { UsersList } from './data/users-list';
+import { FilterOptions } from './interfaces/filter-options.interface';
+import { isWithinInterval } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +11,63 @@ import { UsersList } from './data/users-list';
 })
 export class AppComponent implements OnInit {
   usersList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
   
   ngOnInit(): void {
     setTimeout(() => {
       this.usersList = UsersList;
-    }, 3000);
+      this.usersListFiltered = UsersList;
+    }, 1500);
   }
 
   onUserSelected(user: IUser){
     this.userSelected = user;
     this.showUserDetails = true;
+  }
+
+  onFilter(filterOptions: FilterOptions){
+    this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
+  }
+
+  filterUsersList(filterOptions: FilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+    filteredList = this.filterUsersListByStatus(filterOptions.status, filteredList);
+    filteredList = this.filterUsersListByDate(filterOptions.startDate, filterOptions.endDate, filteredList);
+
+    return filteredList;
+  }
+  
+  filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined || name === '';
+    
+    if(NAME_NOT_TYPPED)
+      return usersList;
+    
+    return usersList.filter(item => item.nome.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
+  }
+  
+  filterUsersListByStatus(status: boolean | undefined, usersList: IUser[]): IUser[] {
+    const STATUS_NOT_TYPPED = status === undefined;
+
+    if(STATUS_NOT_TYPPED)
+      return usersList;
+
+    return usersList.filter(item => item.ativo === status);
+  }
+
+  filterUsersListByDate(startDate: Date | undefined, endDate: Date | undefined, usersList: IUser[]): IUser[] {
+    const DATES_NOT_TYPPED = startDate === undefined || endDate === undefined;
+
+    if(DATES_NOT_TYPPED)
+      return usersList;
+
+    return usersList.filter(item => isWithinInterval(new Date(item.dataCadastro), {
+      start: startDate,
+      end: endDate
+    }));
   }
 }
--- Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Fundamentos-do-Angular/users-filter-project/src/app/components/filter/filter.component.ts
+++ Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Fundamentos-do-Angular/users-filter-project/src/app/components/filter/filter.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FilterOptions } from 'src/app/interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-filter',
@@ -6,10 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
-  foods = [
-    {
-      value: 'Teste',
-      viewValue: 'Teste'
-    }
+
+  @Output('onFilter') onFilterEmitt = new EventEmitter<FilterOptions>();
+
+  filterOptions: FilterOptions = { 
+    name: undefined,
+    startDate: undefined,
+    endDate: undefined,
+    status: undefined
+  }
+
+  statusList = [
+    { description: 'Todos', value: undefined },
+    { description: 'Ativo', value: true },
+    { description: 'Inativo', value: false }
   ]
+
+  onFilter(){
+    this.onFilterEmitt.emit(this.filterOptions);
+  }
+
 }
