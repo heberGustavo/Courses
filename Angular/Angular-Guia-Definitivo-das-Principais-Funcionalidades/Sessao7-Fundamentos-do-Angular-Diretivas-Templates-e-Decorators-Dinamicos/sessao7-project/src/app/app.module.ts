a307d60fcc8abc02d85e77155692edf5e5c50006
heberGustavo <heber.gbarbosa@hotmail.com>
2026-02-03 12:31:49 -0300

    Template Variables: Criação e Change Detaction

--- Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Sessao7-Fundamentos-do-Angular-Diretivas-Templates-e-Decorators-Dinamicos/sessao7-project/src/app/app.module.ts
+++ Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Sessao7-Fundamentos-do-Angular-Diretivas-Templates-e-Decorators-Dinamicos/sessao7-project/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TemplateVariableChangeDetactionComponent } from './components/template-variable-change-detaction/template-variable-change-detaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateVariableChangeDetactionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
