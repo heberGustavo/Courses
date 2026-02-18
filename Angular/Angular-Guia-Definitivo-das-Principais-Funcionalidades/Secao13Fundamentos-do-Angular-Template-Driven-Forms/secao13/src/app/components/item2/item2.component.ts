7d1451f715c470793c0ee7988df198825964a398
heberGustavo <heber.gbarbosa@hotmail.com>
2026-02-18 15:45:50 -0300

    Verificar estado e acessar a instancia do FormControl dentro da classe do Componente

---
+++ Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Secao13Fundamentos-do-Angular-Template-Driven-Forms/secao13/src/app/components/item2/item2.component.ts
@@ -0,0 +1,22 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-item2',
+  templateUrl: './item2.component.html',
+  styleUrl: './item2.component.scss'
+})
+export class Item2Component implements AfterViewInit{
+  
+  @ViewChild('meuInput1') meuInput1!: NgModel;
+
+  ngAfterViewInit(): void {
+    console.log(this.meuInput1);
+  }
+
+  send(){
+    if(this.meuInput1.valid && this.meuInput1.touched){
+      console.log('Formulário válido e tocado');
+    }
+  }
+}
