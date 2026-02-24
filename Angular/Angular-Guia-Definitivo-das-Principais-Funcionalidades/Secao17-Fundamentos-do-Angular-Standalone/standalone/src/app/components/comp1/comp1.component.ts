9087d2f8a7d4796ab2f4adae7586c8829d35409a
heberGustavo <heber.gbarbosa@hotmail.com>
2026-02-24 14:09:45 -0300

    Pipes e Directives

--- Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Secao17-Fundamentos-do-Angular-Standalone/standalone/src/app/components/comp1/comp1.component.ts
+++ Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Secao17-Fundamentos-do-Angular-Standalone/standalone/src/app/components/comp1/comp1.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TransformTextPipe } from '../../pipes/transform-text.pipe';
+import { ChangeTextColorDirective } from "../../directives/change-text-color.directive";
 
 @Component({
   selector: 'app-comp1',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TransformTextPipe, ChangeTextColorDirective],
   templateUrl: './comp1.component.html',
   styleUrl: './comp1.component.scss'
 })
---
+++ Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Secao17-Fundamentos-do-Angular-Standalone/standalone/src/app/pipes/transform-text.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'transformText',
+  standalone: true
+})
+export class TransformTextPipe implements PipeTransform {
+
+  transform(value: string): string {
+    return value.toLocaleUpperCase();
+  }
+
+}
