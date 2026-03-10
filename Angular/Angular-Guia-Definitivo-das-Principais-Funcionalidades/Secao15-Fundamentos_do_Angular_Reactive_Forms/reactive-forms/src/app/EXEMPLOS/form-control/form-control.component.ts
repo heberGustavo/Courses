133fa70552223a09670ce224c2176ca84c4b2977
heberGustavo <heber.gbarbosa@hotmail.com>
2026-03-10 07:13:59 -0300

    Ações no form-control

--- Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Secao15-Fundamentos_do_Angular_Reactive_Forms/reactive-forms/src/app/EXEMPLOS/form-control/form-control.component.ts
+++ Angular/Angular-Guia-Definitivo-das-Principais-Funcionalidades/Secao15-Fundamentos_do_Angular_Reactive_Forms/reactive-forms/src/app/EXEMPLOS/form-control/form-control.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control',
   templateUrl: './form-control.component.html',
-  styleUrl: './form-control.component.scss'
+  styleUrl: './form-control.component.scss',
 })
-export class FormControlComponent {
+export class FormControlComponent implements OnInit {
+  nome = new FormControl({ value: '', disabled: false });
+  
+  ngOnInit(){
+    console.log(this.nome);  
+  }
+  
+  onClickMostrarNomeControl(){
+    console.log(this.nome);
+  }
+
+  onClickAlterarValor() {
+    this.nome.setValue('Fulano');
+  }
+
+  nomeAlterado(){
+    console.log(this.nome.value);
+  }
+
+  desabilitar(){
+    this.nome.disable();
+  }
+
+  habilitar(){
+    this.nome.enable();
+  }
 
 }
